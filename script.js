b2cfb93a846a418f9f036326f8625f4656e5706a
hashim <hashim@recruitbpm.com>
2018-12-24 17:35:02 +0500

    u

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $(function() {
-    $('#Date').datepicker();
-    $('#Time').timepicker();
+    $('#Date').datepicker({
+        uiLibrary: 'material'
+    });
+    $('#Time').timepicker({
+        uiLibrary: 'material',
+        format: 'HH.MM',
+        // mode: 'ampm',
+        modal: false,
+        header: true,
+        footer: true,
+        size: 'small'
+    });
     $("#Color").spectrum({
         color: "#007bff"
     });
@@ -8,38 +18,40 @@ $(function() {
     $(document).ready(function () {
         let data, grid, dialog;
         data = [
-            { 'ID': 1, 'Title': 'Meet Gabriel', 'Date': '12/25/2018', 'Time': '11:00' },
-            { 'ID': 2, 'Title': 'Buy Grcoery', 'Date': '12/28/2018', 'Time': '12:26' },
-            { 'ID': 3, 'Title': 'Pay Bill', 'Date': '11/12/2018', 'Time': '07:00' },
+            { 'ID': 1, 'Title': 'Meet Gabriel', 'Date': '12/25/2018', 'Time': '08:57' },
+            { 'ID': 2, 'Title': 'Buy Grcoery', 'Date': '12/28/2018', 'Time': '12.20' },
+            { 'ID': 3, 'Title': 'Pay Bill', 'Date': '11/12/2018', 'Time': '18.30' },
         ];
         grid = $('#grid').grid({
             primaryKey: 'ID',
-            dataSource: data,
-            uiLibrary: 'bootstrap4',
+            // dataSource: data,
+            uiLibrary: 'material',
             columns: [
                 { field: 'ID',  title: 'ID', width: 56, sortable: true },
-                { field: 'Title', title: 'Title', sortable: true },
-                { field: 'Date', title: 'Date', sortable: true },
-                { field: 'Time', title: 'Time', sortable: true },
-                { field: 'Edit', width: 70, tmpl: '<i class="far fa-edit"></i>', align: 'center', events: { 'click': Edit } },
-                { field: 'Delete', width: 80, tmpl: '<i class="far fa-trash-alt"></i>', align: 'center', events: { 'click': Delete } }
+                { field: 'Title', sortable: true, editor: true },
+                { field: 'Date', type: 'date', sortable: true, editor: true},
+                { field: 'Time', type: 'time', sortable: true, editor: true },
+                // { field: 'Edit', width: 70, tmpl: '<i class="far fa-edit"></i>', align: 'center', events: { 'click': Edit } },
+                // { field: 'Delete', width: 80, tmpl: '<i class="far fa-trash-alt"></i>', align: 'center', events: { 'click': Delete } }
             ],
-            pager: { limit: 10, sizes: [10, 20, 50, 100] }
+            pager: { limit: 10, sizes: [10, 20, 50, 100] },
+            inlineEditing: { mode: 'command' }
         });
-        dialog = $('#dialog').dialog({
-            autoOpen: false,
-            resizable: false,
-            uiLibrary: 'bootstrap4',
-            iconsLibrary: 'fontawesome',
-            modal: true,
-            width: 360
-        });
-        function Edit(e) {
-            $('#ID').val(e.data.id);
-            $('#Title').val(e.data.record.Title);
-            $('#Date').val(e.data.record.Date);
-            dialog.open('Edit Player');
-        }
+        // dialog = $('#dialog').dialog({
+        //     autoOpen: false,
+        //     resizable: false,
+        //     uiLibrary: 'bootstrap4',
+        //     iconsLibrary: 'fontawesome',
+        //     modal: true,
+        //     width: 360,
+        // });
+
+        // function Edit(e) {
+        //     $('#ID').val(e.data.id);
+        //     $('#Title').val(e.data.record.Title);
+        //     $('#Date').val(e.data.record.Date);
+        //     // dialog.open('Edit Player');
+        // }
         function Delete(e) {
             if (confirm('Are you sure?')) {
                 grid.removeRow(e.data.id);
@@ -58,7 +70,7 @@ $(function() {
                 $('#Date').val('');
                 $('#Time').val('');
                 $('[data-position = ' + id + ']').css("background-color",  color);
-                console.log(data );
+                console.log($('#Time'));
             }
             // dialog.close();
         }
