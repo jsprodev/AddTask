ae3e24ccd6df3f4040bbd11269b3ea0d36ff15f6
hashim <hashim@recruitbpm.com>
2018-12-26 19:14:27 +0500

    u

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
-  $('#username').editable();
+$(function () {
+
+  $('#btnSave').on('click', function(){
+    let title = $('#title').val();
+    let date = $('#date').val();
+    let time = $('#time').val();
+    $('#taskListing').append('<li><span><a id="e-title" data-type="text">' + title + '</a></span>' + '</li>');
+    $('#taskListing').append('<li><span><a id="e-date"  data-type="date">' + date + '</a></span>' + '</li>');
+    $('#taskListing').append('<li><span><a id="e-time" data-type="time">' + time + '</a></span>' + '</li>');
+
+    $('#e-title').editable({
+    });
+
+    $('#e-date').editable({
+    });
+
+    $('#e-time').editable({
+    });
+
+  });
+
+  
+  
+
+
 });
