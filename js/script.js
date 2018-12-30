d7919560993e0d5736bcfb19ba444c81bd5660b1
hbk <hbk_047@live.com>
2018-12-30 23:27:24 +0500

    u

--- js/script.js
+++ js/script.js
@@ -9,17 +9,30 @@ $(function () {
     $('#taskListing').append('<li><span><a id="e-time" data-type="time">' + time + '</a></span>' + '</li>');
 
     $('#e-title').editable({
+      mode: 'inline'
     });
 
     $('#e-date').editable({
+      mode: 'inline'
     });
 
     $('#e-time').editable({
+      mode: 'inline'
     });
 
   });
 
-  
+  $('#dob').editable({
+    mode: 'inline',
+    format: 'YYYY-MM-DD',    
+    viewformat: 'DD.MM.YYYY',    
+    template: 'D / MMMM / YYYY',    
+    combodate: {
+            minYear: 2000,
+            maxYear: 2030,
+            minuteStep: 1
+       }
+  });
   
 
 
