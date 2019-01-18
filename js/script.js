0abc0791aece58483d97284dd00ea0b1d05413b4
hashim <hashim@recruitbpm.com>
2019-01-18 18:23:20 +0500

    started working

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ $(function () {
     let date = $('#date').val();
     let time = $('#time').val();
     $('#taskListing').append('<li><span><a id="e-title" data-type="text">' + title + '</a></span>' + '</li>');
-    $('#taskListing').append('<li><span><a id="e-date"  data-type="date">' + date + '</a></span>' + '</li>');
-    $('#taskListing').append('<li><span><a id="e-time" data-type="time">' + time + '</a></span>' + '</li>');
+    $('#taskListing').append('<li><span><a id="e-date" data-type="combodate" data-format="DD-MMM-YYYY" data-template="DD MMM YYYY">' + date + '</a></span>' + '</li>');
+    $('#taskListing').append('<li><span><a id="e-time" data-type="combodate" data-format="hh:mm a" data-template="hhmma">' + time + '</a></span>' + '</li>');
+
+    // <a href="#" id="dob" data-type="combodate" data-value="1984-05-15" data-format="YYYY-MM-DD" data-viewformat="DD/MM/YYYY" data-template="D / MMM / YYYY" data-pk="1" data-title="Select Date of birth" class="editable editable-click editable-open" data-original-title="" title="">15/05/1984</a>
 
     $('#e-title').editable({
       mode: 'inline'
@@ -22,16 +24,19 @@ $(function () {
 
   });
 
-  $('#dob').editable({
-    mode: 'inline',
-    format: 'YYYY-MM-DD',    
-    viewformat: 'DD.MM.YYYY',    
-    template: 'D / MMMM / YYYY',    
-    combodate: {
-            minYear: 2000,
-            maxYear: 2030,
-            minuteStep: 1
-       }
+  $(function(){
+    $('#date').combodate({
+      minYear: 2010,
+      maxYear: 2020,
+      firstItem: 'name'
+    });    
+  });
+
+  $(function(){
+    $('#time').combodate({
+        firstItem: 'name', //show 'hour' and 'minute' string at first item of dropdown
+        minuteStep: 10,
+    });  
   });
   
 
