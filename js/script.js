a575b02fadde47c2680aab2a948ec4b5fb136637
hashim <hashim@recruitbpm.com>
2019-01-24 17:36:51 +0500

    u

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 
-$(function () {
+$(function(){
+
+  $('[data-toggle="tooltip"]').tooltip();
+  
   let tasks = [];
+  
   $('#btnSave').on('click', function(){
     let title = $('#title').val();
     let date = $('#date').val();
@@ -8,38 +12,41 @@ $(function () {
     let arr = [title, date, time];
     tasks.push(arr);
     console.log(tasks);
+    $('#taskListing').append('<div class="col-sm-3">' +
+                                '<div class="card">' +
+                                  '<div class="card-header">' +
+                                    '<a class="e-title" data-type="text">' + title + '</a><span class="tasks-actions float-right"><a class=" task-actions-edit" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-pen-fancy"></i></a> <a id="del" class="task-actions-delete" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash"></i></a></span>' +
+                                  '</div>' +
+                                  '<ul class="list-group list-group-flush">' +
+                                    '<li class="list-group-item"><a class="e-date" data-type="combodate" data-format="DD-MMM-YYYY" data-template="DD MMM YYYY">' + date + '</a></li>' +
+                                    '<li class="list-group-item"><a class="e-time" data-type="combodate" data-format="hh:mm a" data-template="hhmma">' + time + '</a></li>' +
+                                  '</ul>' +
+                                '</div>' +
+                              '</div>');
 
-    $('#taskListing').append(`<div class="col-sm-3">
-                                <div class="card">
-                                  <div class="card-header">
-                                    <a class="e-title" data-type="text">` + title + `</a>
-                                  </div>
-                                  <ul class="list-group list-group-flush">
-                                    <li class="list-group-item"><a class="e-date" data-type="combodate" data-format="DD-MMM-YYYY" data-template="DD MMM YYYY">` + date + `</a></li>
-                                    <li class="list-group-item"><a class="e-time" data-type="combodate" data-format="hh:mm a" data-template="hhmma">` + time + `</a></li>
-                                  </ul>
-                                </div>
-                              </div>`);
 
-    // $('#taskListing').append('<li><span><a class="e-title" data-type="text">' + title + '</a></span>' + '</li>');
-    // $('#taskListing').append('<li><span><a class="e-date" data-type="combodate" data-format="DD-MMM-YYYY" data-template="DD MMM YYYY">' + date + '</a></span>' + '</li>');
-    // $('#taskListing').append('<li><span><a class="e-time" data-type="combodate" data-format="hh:mm a" data-template="hhmma">' + time + '</a></span>' + '</li>');
+    });
+                 
+    
+    let del = document.getElementsByClassName('task-actions-delete')
+    var myFunction = function() {
+      alert('hi');
+   };
+    for (let i = 0; i < del.length; i++) {
+      del[i].addEventListener('click', myFunction, false);
+    }
 
-    // <a href="#" id="dob" data-type="combodate" data-value="1984-05-15" data-format="YYYY-MM-DD" data-viewformat="DD/MM/YYYY" data-template="D / MMM / YYYY" data-pk="1" data-title="Select Date of birth" class="editable editable-click editable-open" data-original-title="" title="">15/05/1984</a>
-
+  function editable() {
     $('.e-title').editable({
       mode: 'inline'
     });
-
     $('.e-date').editable({
       mode: 'inline'
     });
-
     $('.e-time').editable({
       mode: 'inline'
     });
-
-  });
+  }
 
   $(function(){
     $('#date').combodate({
@@ -56,5 +63,4 @@ $(function () {
     });  
   });
 
-});
-
+})
