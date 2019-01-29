bb5664b183c4ec097320dbd4df61be80b623c6ba
hbk <hbk_047@live.com>
2019-01-29 23:06:33 +0500

    u

--- js/script.js
+++ js/script.js
@@ -14,28 +14,30 @@ $(function() {
       id = id + 1;
       let obj = {"id": id, "title": title, "date": date, "time": time};
       tasks.push(obj);
-      console.log(tasks);
-      $('#taskListing').append('<div class="col-sm-4 lol">' +
-                                  '<div class="card">' +
-                                    '<div class="card-header">' +
-                                      '<a class="e-title" data-type="text">' + title + '</a><span class="float-right"><a class="task-actions" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-pen-fancy task-actions-edit"></i></a><a class="task-actions" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash task-actions-delete"></i></a></span>' +
-                                    '</div>' +
-                                    '<ul class="list-group list-group-flush">' +
-                                      '<li class="list-group-item"><a class="e-date" data-type="combodate" data-format="DD-MMM-YYYY" data-template="DD MMM YYYY">' + date + '</a></li>' +
-                                      '<li class="list-group-item"><a class="e-time" data-type="combodate" data-format="hh:mm a" data-template="hh mm a">' + time + '</a></li>' +
-                                    '</ul>' +
-                                  '</div>' +
-                                '</div>');
+      let html = '<div class="col-sm-4" id="task-%id%">' +
+                    '<div class="card">' +
+                      '<div class="card-header">' +
+                        '<a class="e-title" data-type="text">' + title + '</a><span class="float-right"><a class="task-actions" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-pen-fancy task-actions-edit"></i></a><a class="task-actions" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash task-actions-delete"></i></a></span>' +
+                      '</div>' +
+                      '<ul class="list-group list-group-flush">' +
+                        '<li class="list-group-item"><a class="e-date" data-type="combodate" data-format="DD-MMM-YYYY" data-template="DD MMM YYYY">' + date + '</a></li>' +
+                        '<li class="list-group-item"><a class="e-time" data-type="combodate" data-format="hh:mm a" data-template="hh mm a">' + time + '</a></li>' +
+                      '</ul>' +
+                    '</div>' +
+                  '</div>';
+      let newHtml = html.replace('%id%', id);
+      // $('#taskListing').append(newHtml);
+      document.querySelector('#taskListing').insertAdjacentHTML('beforeend', newHtml);
       $('#title').val('');
+      console.log(tasks);
     } else {
       alert('Title is required');
     }
-    
     });
                  
     document.querySelector('#taskListing').addEventListener('click', function(e) {
       if (event.target.classList.contains('task-actions-delete')) {
-        console.log(e.value);
+      
       }
     });
 
