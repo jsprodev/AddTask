c8019b4953599c56af2e69db6c8fa92c7d5ffdf7
hashim <hashim@recruitbpm.com>
2019-02-04 17:20:06 +0500

    u

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ $(function() {
       id = id + 1;
       let obj = {"id": id, "title": title, "date": date, "time": time};
       tasks.push(obj);
-      let html = '<div class="col-sm-4" id="task-%id%">' +
-                    '<div class="card">' +
+      let html = '<div class="col-sm-4">' +
+                    '<div class="card" id="task-%id%">' +
                       '<div class="card-header">' +
                         '<a class="e-title" data-type="text">' + title + '</a><span class="float-right"><a class="task-actions" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-pen-fancy task-actions-edit"></i></a><a class="task-actions" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash task-actions-delete"></i></a></span>' +
                       '</div>' +
@@ -29,18 +29,37 @@ $(function() {
       // $('#taskListing').append(newHtml);
       document.querySelector('#taskListing').insertAdjacentHTML('beforeend', newHtml);
       $('#title').val('');
-      console.log(tasks);
+      console.log('saved: ', tasks);
     } else {
       alert('Title is required');
     }
     });
-                 
+     
+    // delete task
     document.querySelector('#taskListing').addEventListener('click', function(e) {
       if (event.target.classList.contains('task-actions-delete')) {
-      
+        let element, parent, elementID, ids, index;
+
+        element = event.target.parentNode.parentNode.parentNode.parentNode;
+        parent = element.parentNode;
+        parent.parentNode.removeChild(parent);
+        element = element.id;
+        element = element.split('-');
+        elementID = parseInt(element[1]);
+
+        ids = tasks.map(function(current) {
+          return current.id; 
+        });
+        
+        index = ids.indexOf(elementID);
+        if (index !== -1) {
+          tasks.splice(index, 1);
+        }
+        console.log(tasks);
       }
     });
 
+    // edit task
     document.querySelector('#taskListing').addEventListener('click', function(e) {
       if (e.target.classList.contains('task-actions-edit')) {
         $('.e-title').editable({
@@ -53,6 +72,13 @@ $(function() {
           mode: 'inline'
         });
       }
+
+      // save edit values
+      let title = $('#title').val();
+      let date = $('#date').val();
+      let time = $('#time').val();
+
+      console.log(title);
     });
 
     $(function(){
