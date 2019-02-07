8c23718af8a7106ea91480d3afd01adda7afc0eb
hashim <hashim@recruitbpm.com>
2019-02-07 18:32:18 +0500

    u

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 
 $(function() {
 
+  // initialize date
+  $('#date').combodate({
+    minYear: 2019,
+    maxYear: 2025,
+    firstItem: 'name'
+  });    
+
+  // initialize time
+  $('#time').combodate({
+      firstItem: 'name', //show 'hour' and 'minute' string at first item of dropdown
+      minuteStep: 10,
+  });
+    
   $('[data-toggle="tooltip"]').tooltip();
   $.fn.editable.defaults.mode = 'inline';
+
+  // initialize color picker
+  $("#color").spectrum({
+    preferredFormat:'rgb',
+    showAlpha: true,
+    showInput: true
+  });
   
   let tasks = [];
   let id = 0;
@@ -12,24 +32,25 @@ $(function() {
       let title = $('#title').val();
       let date = $('#date').val();
       let time = $('#time').val();
+      let color = $('#color').val();
+      console.log(color);
       id = id + 1;
-      let obj = {"id": id, "title": title, "date": date, "time": time};
+      let obj = {"id": id, "title": title, "date": date, "time": time, "color": color};
       tasks.push(obj);
       let html = '<div class="col-sm-4">' +
-                    '<div class="card" id="task-' + id +'">' +
+                    '<div class="card" id="task-' + id +'"  style="background-color: ' + color + ' ">' +
                       '<div class="card-header">' +
                         '<a class="e-title e-editable" data-type="text" data-pk="' + id + '" >' + title + '</a><span class="float-right"><a class="inline-edit task-actions" data-id="' + id + '" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fas fa-pen-fancy task-actions-edit"></i></a><a class="task-actions" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fas fa-trash task-actions-delete"></i></a></span>' +
                       '</div>' +
                       '<ul class="list-group list-group-flush">' +
-                        '<li class="list-group-item"><a class="e-date e-editable" data-type="combodate" data-format="DD-MMM-YYYY" data-template="DD MMM YYYY">' + date + '</a></li>' +
+                        '<li class="list-group-item"><a class="e-date e-editable" data-type="combodate" data-format="DD-MMM-YYYY" data-template="DDMMMYYYY" >' + date + '</a></li>' +
                         '<li class="list-group-item"><a class="e-time e-editable" data-type="combodate" data-format="hh:mm a" data-template="hh mm a">' + time + '</a></li>' +
                       '</ul>' +
                     '</div>' +
                   '</div>';
       
-      newHtml = html;
       // $('#taskListing').append(newHtml);
-      document.querySelector('#taskListing').insertAdjacentHTML('beforeend', newHtml);
+      document.querySelector('#taskListing').insertAdjacentHTML('beforeend', html);
       $('#title').val('');
       console.log('saved: ', tasks);
     } else {
@@ -37,22 +58,19 @@ $(function() {
     }
     });
      
-    // delete task
+    // delete task javascript approach
     document.querySelector('#taskListing').addEventListener('click', function(e) {
       if (event.target.classList.contains('task-actions-delete')) {
         let element, parent, elementID, ids, index;
-
         element = event.target.parentNode.parentNode.parentNode.parentNode;
         parent = element.parentNode;
         parent.parentNode.removeChild(parent);
         element = element.id;
         element = element.split('-');
         elementID = parseInt(element[1]);
-
         ids = tasks.map(function(current) {
           return current.id; 
         });
-        
         index = ids.indexOf(elementID);
         if (index !== -1) {
           tasks.splice(index, 1);
@@ -61,54 +79,29 @@ $(function() {
       }
     });
 
-    // make tasks editable 
-    $('body').on('click', '.inline-edit', function(e){ 
-      
-      var id = $(this).data('id');
-
-      $('#task-' + id + ' .e-editable').each((i, el) => {
-        if($(el).hasClass('editable')){
-          $(el).toggleClass('editable editable-click');
+    // make tasks editable jquery approach
+    $('body').on('click', '.inline-edit', function(e) {
+      let id = $(this).data('id');
+      $('#task-' + id + ' .e-editable').each(function(i, el) { 
+        if($(el).hasClass('editable') && $(el).hasClass('editable-click')) {
+          $(el).removeClass('editable editable-click');
           $(el).editable('destroy');
-          return;
+        } else {
+          if($(el).hasClass('e-date')) {
+            $(el).editable({
+              format: 'DD-MMM-YYYY',    
+              viewformat: 'DD-MMM-YYYY',    
+              template: 'DD/MMM/YYYY',    
+              combodate: {
+                minYear: 2019,
+                maxYear: 2025,
+                firstItem: 'name'
+              }
+            });
+          }
+          $(el).editable();
         }
-
-        $(el).editable();
       });
-    });
-
-    function updateTaskArray(id) {
-      let title = $('.e-title').val();
-      let date = $('.e-date').val();
-      let time = $('.e-time').val();
-      console.log(id + ' ' + title + '' + date + ' ' + time)
-    }
-
-    // document.querySelector('.editable-submit').addEventListener('click', function(e) {
-
-    // });
-
-    $(function(){
-      $('#date').combodate({
-        minYear: 2010,
-        maxYear: 2030,
-        firstItem: 'name'
-      });    
-    });
-
-    $(function(){
-      $('.e-date').combodate({
-        minYear: 2010,
-        maxYear: 2030,
-        firstItem: 'name'
-      });    
-    });
-
-    $(function(){
-      $('#time').combodate({
-          firstItem: 'name', //show 'hour' and 'minute' string at first item of dropdown
-          minuteStep: 10,
-      });  
     });
 
 })
